b0ad51b770c298f7be2573e40de3be5b9d68661b
anjosdobem <anjosdobem18@gmail.com>
2018-11-04 15:15:25 +0000

    I Finished project.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,9 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+  # config/environments/production.rb
 
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
@@ -88,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+
 end
